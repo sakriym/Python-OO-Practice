43453b0144da3677edf8651d51982c33d3aa4726
sakriymohamed <sakriymohamed@gmail.com>
2023-07-18 15:31:34 -0700

    updated serial.py

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+#
+    def __init__(self, start=0):
+        """Initialize with a start number and create a current Num to hold start"""
+        self.start = start
+        self.currentNum = start
+
+    def generate(self):
+        """Create a variable to hold current Num, increment CurrentNum,
+            return num
+        """
+        num = self.currentNum
+        self.currentNum += 1
+        return num
+
+    def reset(self):
+        """Reassign currentNum to the start number"""
+        self.currentNum = self.start
